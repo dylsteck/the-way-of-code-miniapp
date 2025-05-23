1f162c1ea8e523131e86f603b2d1324ad9fa2904
dylsteck <dylancsteck@gmail.com>
2025-05-23 14:08:24 -0400

    fix Demo to use iframe on cbw because of sdk.context(or router.push) error

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,20 +1,32 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { sdk } from "@farcaster/frame-sdk";
 
 export default function Demo() {
   const router = useRouter();
+  const [isCBW, setIsCBW] = useState<boolean>(false);
+  const [isInMiniApp, setIsInMiniApp] = useState<boolean>(false);
+
   useEffect(() => {
     const init = async() => {
-      const isInMiniApp = await sdk.isInMiniApp();
-      if(isInMiniApp){
+      const inMiniApp = await sdk.isInMiniApp();
+      const context = await sdk.context;
+      if(inMiniApp && context){
         await sdk.actions.ready();
-        router.push("https://www.thewayofcode.com/");
+        setIsCBW(context.client.clientFid === 399519);
       }
+      setIsInMiniApp(inMiniApp)
     }
     init();
-  }, [router]);
+  }, [isCBW, isInMiniApp]);
+
+  if(isInMiniApp && isCBW){
+    return <iframe src="https://www.thewayofcode.com/" className="w-screen h-screen border-0" />
+  } else if(isInMiniApp && !isCBW){
+    router.push("https://www.thewayofcode.com/");
+  }
+
   return null;
 }
